05146c707de6075ad8b7b6974a5f35ead9ccfbdd
ely.nguyen <ely.nguyen@tpfgroup.cc>
2023-03-10 16:06:30 +0700

    buoi 1

--- BigSchool/ViewModels/CourseViewModel.cs
+++ BigSchool/ViewModels/CourseViewModel.cs
@@ -12,10 +12,12 @@ namespace BigSchool.ViewModels
         [Required]
         public string Place { get; set; }
         [Required]
+        [FutureDate]
         public string Date { get; set; }
         [Required]
         public string Time { get; set; }
         [Required]
+        [Validtime]
         public byte Category { get; set; }
         public IEnumerable<Category> categories { get; set; }
         public DateTime GetDateTime()
---
+++ BigSchool/ViewModels/Validtime.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace BigSchool.ViewModels
+{
+    public class Validtime:ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            DateTime dateTime;
+            var isVaild = DateTime.TryParseExact(Convert.ToString(value),
+                 "dd/M/yyy",
+                 CultureInfo.CurrentCulture,
+                 DateTimeStyles.None,
+                 out dateTime
+                );
+            return isVaild;
+        }
+    }
+}
